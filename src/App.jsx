3a44ee6175f830a99f9f0c9d617f5bbfb3d6408a
gokul <gokulchandan24@gmail.com>
2024-09-29 00:07:36 +0530

    created UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Camp from "./components/Camp";
+import Features from "./components/Features";
+import GetApp from "./components/GetApp";
+import Guide from "./components/Guide";
+import Hero from "./components/Hero";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click</Button>
+      <main className="relative overflow-hidden">
+        <Hero />
+        <Camp />
+        <Guide />
+        <Features />
+        <GetApp />
+      </main>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./create-trip/index.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
+  </StrictMode>
+);
